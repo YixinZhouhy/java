081f05841aac157a4503cbb486dd44bf435f9e45
YixinZhouhy <yixinzhouhy@gmail.com>
2017-07-20 09:01:53 +0800

    循环及数组

---
+++ Topic3/src/Topic3/Topic3.java
@@ -0,0 +1,57 @@
+/*
+ *  数组
+ */
+
+
+package Topic3;
+
+import java.util.Scanner;
+
+public class Topic3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner in = new Scanner(System.in);
+		
+		int x;
+		double sum = 0;
+		int cnt = 0;
+		int[] numbers = new int [100];
+		
+		x = in.nextInt();
+		
+//		读取数组以-1 结束
+		while( x != -1)
+		{
+			numbers[cnt] = x;
+			sum += x;
+			cnt++;
+			x = in.nextInt();
+		}
+
+		
+//		若数组中有数据存入则计算平均值，并打印大于平均值的数
+		if( cnt > 0)
+		{
+			double average = sum / cnt;	
+			
+			System.out.println(average);
+			
+			for(int i = 0, j = 0; i < cnt; i++ )
+			{
+				if(numbers[i] > average)
+				{
+					System.out.println("the number(s) bigger than the average :");
+					System.out.println(numbers[i]);
+					j++;
+				}
+				if(j == 0)
+				{
+					System.out.println("There is no number bigger than average");
+				}
+			}
+		}
+		
+	}
+
+}
